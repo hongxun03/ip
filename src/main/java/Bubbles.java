7898e35fa2bfe3e434e8baf0eb9d6a37549c6490
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-08-15 15:32:17 +0800

    Renamed chatbot, made chatbot greet and exit the user

---
+++ src/main/java/Bubbles.java
@@ -0,0 +1,11 @@
+public class Bubbles {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String name = "Bubbles";
+        System.out.println(line + "\nHello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+
+    }
+}
