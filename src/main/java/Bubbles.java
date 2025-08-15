0939e7dec6de9f5238a2ae4f7120f19e3458b15b
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-08-15 22:44:37 +0800

    Completed Level 4, adding support for todo, deadline and event. Also separated mark and unmark into separate functions

--- src/main/java/Bubbles.java
+++ src/main/java/Bubbles.java
@@ -1,3 +1,5 @@
+import task.*;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,7 +29,7 @@ public class Bubbles {
             message = scanner.nextLine().trim();
             if (message.isEmpty()) continue;
 
-            String[] parts = message.split(" ");
+            String[] parts = message.split(" ", 2);
             String command = parts[0];
             String arg = (parts.length > 1) ? parts[1] : "";
 
@@ -36,23 +38,40 @@ public class Bubbles {
                     listTasks();
                     break;
                 case "mark":
-                    taskList.get(Integer.parseInt(arg) - 1).setCompleted();
+                    markTask(command, Integer.parseInt(arg));
                     break;
                 case "unmark":
-                    taskList.get(Integer.parseInt(arg) - 1).unComplete();
+                    unMarkTask(command, Integer.parseInt(arg));
                     break;
                 case "bye":
                     bye();
                     return;
                 default:
-                    addTask(message);
+                    addTask(command, arg);
             }
         }
     }
 
-    public void addTask(String message) {
-        taskList.add(new Task(message));
-        System.out.println(LINE + "\n\t added: " + message + "\n" + LINE);
+    public void addTask(String command, String arg) {
+        switch (command) {
+            case "todo":
+                taskList.add(new ToDo(arg));
+                break;
+            case "deadline":
+                String[] split1 = arg.split("/by ");
+                taskList.add(new Deadline(split1[0], split1[1]));
+                break;
+            case "event":
+                String[] fromSplit = arg.split("/from ");
+                String[] bySplit = fromSplit[1].split("/to ");
+                taskList.add(new Event(fromSplit[0], bySplit[0], bySplit[1]));
+                break;
+        }
+        int listSize = taskList.size();
+        System.out.println(LINE + "\n\t Got it. I've added this task:");
+        System.out.println("\t\t" + taskList.get(listSize - 1).toString());
+        System.out.println("\t Now you have " + listSize + (listSize == 1 ? " task" : " tasks")
+                + " in the list.\n" + LINE);
     }
 
     public void listTasks() {
@@ -61,6 +80,20 @@ public class Bubbles {
         for (int i = 0; i < taskList.size(); i++) {
             System.out.println("\t " + (i + 1) + ". " + taskList.get(i).toString());
         }
+        System.out.println(LINE);
+    }
+
+    public void markTask(String command, int arg) {
+        taskList.get(arg - 1).setCompleted();
+        System.out.println(LINE);
+        System.out.println("\t Nice! I've marked this task as done:\n\t\t" + taskList.get(arg - 1).toString());
+        System.out.println(LINE);
+    }
+
+    public void unMarkTask(String command, int arg) {
+        taskList.get(arg - 1).unComplete();
+        System.out.println(LINE);
+        System.out.println("\t OK, I've marked this task as not done yet:\n\t\t" + taskList.get(arg - 1).toString());
         System.out.println(LINE);
     }
 
