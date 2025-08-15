f6c03f6468798573da32eb6b042d4662dc4fb649
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-08-15 22:42:30 +0800

    Removed printing statements

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -12,16 +12,10 @@ public class Task {
 
     public void setCompleted() {
         this.completed = true;
-        System.out.println(LINE);
-        System.out.println("\t Nice! I've marked this task as done:\n\t\t" + this);
-        System.out.println(LINE);
     }
 
     public void unComplete() {
         this.completed = false;
-        System.out.println(LINE);
-        System.out.println("\t OK, I've marked this task as not done yet:\n\t\t" + this);
-        System.out.println(LINE);
     }
 
     public String toString() {
