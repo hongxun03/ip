97b66201f4359021e2eeb24e2e2d60ba5e874403
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-08-15 16:11:48 +0800

    Completed Level 1, placed code into functions to improve functionality

--- src/main/java/Bubbles.java
+++ src/main/java/Bubbles.java
@@ -1,18 +1,39 @@
-public class Bubbles {
-    public static String line = "\t____________________________________________________________";
-    public static String name = "Bubbles";
+import java.util.Scanner;
 
-    public static void Greetings() {
-        System.out.println(line + "\n\tHello! I'm " + name);
-        System.out.println("\tWhat can I do for you?\n" + line);
+public class Bubbles {
+    private static final String LINE = "\t____________________________________________________________";
+    private final String name;
+
+    public Bubbles(String name) {
+        this.name = name;
     }
 
-    public static void Bye() {
-        System.out.println("\tBye. Hope to see you again soon!\n" + line);
+    public void greetings() {
+        System.out.println(LINE + "\n\tHello! I'm " + name + " \uD83E\uDEE7\uD83E\uDEE7");
+        System.out.println("\tWhat can I do for you?\n" + LINE);
+    }
+
+    public void bye() {
+        System.out.println(LINE + "\n\tBye. Hope to see you again soon!\n" + LINE);
+    }
+
+    public void echo() {
+        Scanner scanner = new Scanner(System.in);
+        String message;
+        while (true) {
+            message = scanner.nextLine().trim();
+            if (message.equals("bye")) {
+                scanner.close();
+                break;
+            }
+            System.out.println(LINE + "\n\t" + message + "\n" + LINE);
+        }
     }
 
     public static void main(String[] args) {
-        Greetings();
-        Bye();
+        Bubbles bubbles = new Bubbles("Bubbles");
+        bubbles.greetings();
+        bubbles.echo();
+        bubbles.bye();
     }
 }
