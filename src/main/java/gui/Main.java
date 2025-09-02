fe06651e74cfd3d1469b88c089535bdba370fdac
hongxun03 <119728307+hongxun03@users.noreply.github.com>
2025-09-02 16:51:27 +0800

    Copy code from JavaFX Tutorial

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,35 @@
+package gui;
+
+import java.io.IOException;
+
+import chatbot.Bubbles;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Bubbles using FXML.
+ */
+public class Main extends Application {
+
+    private Bubbles bubbles = new Bubbles("./data/Bubbles.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            stage.setMaxWidth(800);
+            fxmlLoader.<MainWindow>getController().setDuke(bubbles); // inject the Bubbles instance
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
